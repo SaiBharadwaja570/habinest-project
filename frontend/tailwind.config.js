3e3da98474dddb1042547842824c27eb81678f43
chaitanya bunny <chaitanyagunjapalli@gmail.com>
2025-05-10 10:35:00 +0530

    added both of the config files and tailwinf config and ts config files

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,14 @@
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        drab: "#504B3A",
+        asparagus: "#69995D",
+        timberwolf: "#E4DFDA",
+        azure: "#007FFF",
+      },
+    },
+  },
+  plugins: [],
+};
