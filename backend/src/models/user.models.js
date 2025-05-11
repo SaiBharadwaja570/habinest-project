710906cf2e05b8aaca60746639da089920d92838
SaiBharadwaja570 <saibharadwaja1906@gmail.com>
2025-05-11 10:59:36 +0530

    Added logout functionality (completed)

--- backend/src/models/user.models.js
+++ backend/src/models/user.models.js
@@ -48,7 +48,7 @@ userSchema.methods.isPasswordCorrect = async function (password) {
     return await bcrypt.compare(password, this.password);
 };
 
-userSchema.methods.generateAccessToken = async function(){
+userSchema.methods.generateAccessToken = function(){
     return jwt.sign(
         {
             _id: this._id,
@@ -62,7 +62,7 @@ userSchema.methods.generateAccessToken = async function(){
     )
 }
 
-userSchema.methods.generateRefreshToken = async function(){
+userSchema.methods.generateRefreshToken = function(){
     return jwt.sign(
         {
             _id: this._id
