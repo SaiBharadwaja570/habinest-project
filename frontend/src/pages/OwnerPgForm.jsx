90661d4e939c770a3e2df2937ddaf64ad22d1fa1
SaiBharadwaja570 <saibharadwaja1906@gmail.com>
2025-05-14 15:52:54 +0530

    Revert "Updated cloudinary"
    This reverts commit af4177f1295fccb49fb791de2a468acc43ae7890.

--- frontend/src/pages/OwnerPgForm.jsx
+++ frontend/src/pages/OwnerPgForm.jsx
@@ -147,9 +147,10 @@ export default function OwnerPgForm() {
               onChange={handleChange}
               className="w-full border border-gray-300 p-2 rounded-lg mt-1"
             >
-              <option value="Public">Gents</option>
-              <option value="Private">Women</option>
-              <option value="Girls Only">Coliving</option>
+              <option value="Public">Public</option>
+              <option value="Private">Private</option>
+              <option value="Girls Only">Girls Only</option>
+              <option value="Boys Only">Boys Only</option>
             </select>
           </div>
 
