ec3b6b16a19603d457b34b18c4c92fe97927d9c3
A9RYA6N <aryanburnwal8@gmail.com>
2025-05-19 11:17:27 +0530

    Made SinglePg better

--- frontend/src/pages/SinglePg.jsx
+++ frontend/src/pages/SinglePg.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 import { useParams } from 'react-router-dom'
 import Map from '../components/Map'
+import '../App.css'
 const SinglePg = () => {
     const {id}=useParams()
     console.log(id)
@@ -12,12 +13,21 @@ const SinglePg = () => {
             url:`http://localhost:8000/api/pg/${id}`
         }).then(res=>{console.log(res);setPgData(res.data.data)}).catch(err=>{console.error("ERROR",err)})
     },[])
-    if(pgData){
-        console.log(pgData.location.coordinates)
+    if(!pgData){
+        return(<h2>Loading...</h2>)
     }
     return (
-        <div>
-            {pgData ? <Map coords={pgData.location.coordinates} name={pgData.name}/> : <h2>Loading...</h2>}
+        <div className='pg-container'>
+            <div className="pg-map-wrapper">
+                <Map coords={pgData.location.coordinates} name={pgData.name}/>
+            </div>
+            <div className="pg-details">
+                <h2>{pgData.name}</h2>
+                <p>{pgData.address}</p>
+                <p>Price: ₹{pgData.priceRange}</p>
+                <p>Gender: {pgData.gender}</p>
+                {/* Add more details as needed */}
+            </div>
         </div>
     )
 }
