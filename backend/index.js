650654f4715e4b8b46e0a5f3db1753e838f846fb
A9RYA6N <aryanburnwal8@gmail.com>
2025-05-24 19:39:46 +0530

    Test 2

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,24 @@
-import app from './app.js';
-import dotenv from 'dotenv';
-import userDB from './src/db/userDB.js';
-import listingsDB from './src/db/listingsDB.js';
-
+import app from "./app.js";
+import dotenv from 'dotenv'
+import userDB from "./src/db/userDB.js";
+import listingsDB from "./src/db/listingsDB.js";
+import ApiError from "./src/utils/ApiError.js";
 dotenv.config();
 
-await userDB().then(() => console.log("Connected user")).catch(console.error);
-await listingsDB().then(() => console.log("Connected listing")).catch(console.error);
+app.on('error', (error) => {
+    console.error("Error:", error);
+    throw error;
+})
 
-// ✅ Export the Express app (this is required by Vercel)
-export default app;
+
+
+await userDB().then(res=>{console.log("Connected user")}).catch(err=>{console.log(err)})
+
+await listingsDB().then(res=>{console.log("Connected listing")}).catch(err=>{console.log(err)})
+
+app.listen(process.env.PORT || 8000, () =>{
+    console.log(`Server listening on port: ${process.env.PORT}`)
+})
+
+
+startServer();
