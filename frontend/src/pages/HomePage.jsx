3f45f774373af076378cb1f34e6fbda8514cffc1
SaiBharadwaja570 <saibharadwaja1906@gmail.com>
2025-05-26 15:57:38 +0530

    Updated HomePage

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -14,7 +14,7 @@ const Navbar = () => {
   React.useEffect(() => {
     const checkLoginStatus = async () => {
       try {
-        const res = await axios.get("http://localhost:8000/api/user/check-login", { withCredentials: true });
+        const res = await axios.get(`${import.meta.env.VITE_BACKEND_USER}/`, { withCredentials: true });
         setIsLoggedIn(res.data.loggedIn);
       } catch (error) {
         setIsLoggedIn(false);
@@ -27,7 +27,7 @@ const Navbar = () => {
 
   const handleLogout = async () => {
     try {
-      await axios.post("http://localhost:8000/api/user/logout", {}, { withCredentials: true });
+      await axios.post(`${import.meta.env.VITE_BACKEND_USER}/logout`, {}, { withCredentials: true });
       setIsLoggedIn(false);
       navigate("/");
     } catch (error) {
