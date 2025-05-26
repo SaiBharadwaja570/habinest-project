75ad4d8f497aa18b81232a81c0ebf011bf000ca1
A9RYA6N <aryanburnwal8@gmail.com>
2025-05-26 16:08:22 +0530

    Added visit api

--- backend/src/routes/visit.routes.js
+++ backend/src/routes/visit.routes.js
@@ -1,17 +1,10 @@
-
 import express from 'express'
-import Visit from '../models/visit.models.js';
+import { createVisit, showVisit } from '../controllers/visit.controller.js';
+import { verifyJWT } from '../middlewares/auth.middleware.js';
 const router = express.Router();
 
-router.post("/book", async (req, res) => {
-  try {
-    const { name, phone, date, time, message, pgId } = req.body;
-    const newVisit = new Visit({ name, phone, date, time, message, pgId });
-    await newVisit.save();
-    res.status(201).json({ message: "Visit booked successfully!" });
-  } catch (error) {
-    res.status(500).json({ error: "Failed to book visit" });
-  }
-});
+router.post("/book/:id", createVisit);
+
+router.get("/show", verifyJWT,showVisit)
 
 export default router
