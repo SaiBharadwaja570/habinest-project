d2d62655b8732965e0b9c2d2011ea9c57d32810c
SaiBharadwaja570 <saibharadwaja1906@gmail.com>
2025-05-27 13:31:01 +0530

    Handling errors

--- frontend/src/routes/AppRoutes.jsx
+++ frontend/src/routes/AppRoutes.jsx
@@ -9,7 +9,7 @@ import ProfilePage from "../pages/ProfilePage";
 import PGListPage from "../pages/PGListPage";
 import FilterListingPage from "../pages/FilterListingPage";
 import BookmarkedPGsPage from "../pages/BookmarkedPGsPage";
-import CreatePG from "../paages/CreatePG";
+import CreatePG from "../pages/OwnerPgForm";
 import SinglePg from "../pages/SinglePg";
 import Map from "../components/Map";
 import UpdatePgPage from "../pages/UpdatePgPage";
