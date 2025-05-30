34b053ac8cb627b13f13f296bcb187d3193bbbdd
A9RYA6N <aryanburnwal8@gmail.com>
2025-05-30 11:26:40 +0530

    Update FirstPage.jsx

--- frontend/src/pages/FirstPage.jsx
+++ frontend/src/pages/FirstPage.jsx
@@ -6,7 +6,6 @@ import { useNavigate } from "react-router-dom";
 const FirstPage = () => {
     const [isLoggedIn, setIsLoggedIn] = useState(false);
     const [loading, setLoading] = useState(true);
-    const [isOwner, setIsOwner] = useState(false)
     const navigate=useNavigate();
     useEffect(() => {
         const checkLoginStatus = async () => {
@@ -14,12 +13,7 @@ const FirstPage = () => {
                 const res = await axios.get(`${import.meta.env.VITE_BACKEND_USER}`, { withCredentials: true });
                 setIsLoggedIn(res.data.data);
                 setLoading(false);
-                if(isLoggedIn.type=="owner")
-                {
-                    setIsOwner(true);
-                }
                 localStorage.setItem("isLoggedIn", true)
-                localStorage.setItem("isOwner", true)
             } catch (error) {
                 localStorage.removeItem("isLoggedIn")
                 setIsLoggedIn(false);
@@ -39,7 +33,7 @@ const FirstPage = () => {
 
     return (
         <div>
-            {isLoggedIn ? ( isOwner ? navigate("/pg-list") : navigate('/')) : (navigate('/')) }
+            {isLoggedIn ? ( isLoggedIn.type=="owner" ? navigate("/pg-list") : navigate('/')) : (navigate('/')) }
         </div>
     )
 }
